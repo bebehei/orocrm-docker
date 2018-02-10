db059b2f89cff8b9ae9611c64a30f60fdb8242e6
Benedikt Heine <bebe@bebehei.de>
2018-02-10 15:42:35 +0100

    Use /dev/std{err,out} as nginx logfiles

--- web/files/entrypoint.sh
+++ web/files/entrypoint.sh
@@ -70,9 +70,6 @@ if   [ -r "${SSL_CRT}" ] \
 	nginx -s reload
 fi
 
-touch /var/log/nginx/orocrm.log
-tail -f /var/log/nginx/orocrm.log &
-
 oro-console fos:js-routing:dump
 oro-console oro:localization:dump
 oro-console oro:assets:install
